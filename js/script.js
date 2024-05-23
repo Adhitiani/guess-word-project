51e38e7ff09666c87a7a3fbd4058057b7c3b0b54
Adhitiani Suzanthi <adhitiani@Adhitianis-MacBook-Air.local>
2024-05-23 15:45:54 +0200

    accept and validate input

--- js/script.js
+++ js/script.js
@@ -4,20 +4,60 @@ const guessInput = document.querySelector(".letter");
 const wordProgess = document.querySelector(".word-in-progress");
 const remainingGuess = document.querySelector(".remaining");
 const remainingGuessSpan = document.querySelector(".remaining span");
-const guessMessage = document.querySelector(".message");
+const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
+
+// Display our symbols as placeholders for the chosen word's letters
 
 function addPlaceholderLetter(word) {
   wordProgess.innerText = '● '.repeat(word.length);
 }
 
+addPlaceholderLetter(word);
+
+
 guessButton.addEventListener("click", function (e) {
   e.preventDefault();
+  message.innerText = "";
+
   let inputLetter = guessInput.value;
-  console.log(inputLetter)
+  let goodGuess = validateInput(inputLetter);
+  message.innerText = input;
 
-})
+  if (goodGuess) {
+    makeGuess(input)
+  }
+  inputLetter.value = "";
+});
 
-addPlaceholderLetter(word)
+
+function validateInput(input) {
+  const acceptedLetter = /[a-zA-Z]/;
+  if (!input) {
+    message.innerText = '"please enter a letter."';
+  } else if (input.length !== 1) {
+    message.innerText = '"Please only enter one letter."'
+  } else if (!input.match(acceptedLetter)) {
+    message.innerText = '"please only enter a letter from a to z, no number or symbol."'
+  } else {
+    return input
+  }
+};
+
+
+function makeGuess(letter) {
+  letter = letter.toUpperCase();
+  if (guessedLetters.includes(letter)) {
+    message.innerText = "You have entered the same letter";
+  } else {
+    guessedLetters.push(upperLetter);
+    console.log(guessedLetters);
+  }
+}
+ß
+
+
+
